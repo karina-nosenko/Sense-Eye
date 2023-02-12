f03db58df9c389a82d43071eb69392c38be1fc19
karina-nosenko <karina4nosenko@gmail.com>
2023-02-12 21:18:24 +0200

    cleanup

--- ImageProcessingUnit/main.py
+++ ImageProcessingUnit/main.py
@@ -1,57 +1,55 @@
 import sys
-import argparse
 import math
-import time
-import importlib
 import cv2
 import torch
 import numpy as np
 import torch.backends.cudnn as cudnn
 import itertools
 from numpy import random
-from pathlib import Path
 
-MODE = 'realtime'   # realtime/video
+MODE = 'video'   # realtime/video
 APPEND_PATH = 'yolov7'
 sys.path.append(APPEND_PATH)
 
 from yolov7.models.experimental import attempt_load
-from yolov7.utils.datasets import LoadStreams, LoadImages
-from yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from yolov7.utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
 from yolov7.utils.plots import plot_one_box
-from yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from yolov7.utils.torch_utils import select_device, time_synchronized
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+def adjust_image_to_desired_shape(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
     shape = img.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
 
     # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    scale_ratio = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
     if not scaleup:  # only scale down, do not scale up (for better test mAP)
-        r = min(r, 1.0)
+        scale_ratio = min(scale_ratio, 1.0)
 
     # Compute padding
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    ratio = scale_ratio, scale_ratio  # width, height ratios
+    new_unpad = int(round(shape[1] * scale_ratio)), int(round(shape[0] * scale_ratio))
+    width_padding, height_padding = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
     if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+        width_padding, height_padding = np.mod(width_padding, stride), np.mod(height_padding, stride)
     elif scaleFill:  # stretch
-        dw, dh = 0.0, 0.0
+        width_padding, height_padding = 0.0, 0.0
         new_unpad = (new_shape[1], new_shape[0])
         ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
+    # divide padding into 2 sides
+    width_padding /= 2
+    height_padding /= 2
 
-    if shape[::-1] != new_unpad:  # resize
+    # resize
+    if shape[::-1] != new_unpad:
         img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+
+    top, bottom = int(round(height_padding - 0.1)), int(round(height_padding + 0.1))
+    left, right = int(round(width_padding - 0.1)), int(round(width_padding + 0.1))
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
+    return img, ratio, (width_padding, height_padding)
 
 classes_to_filter = ['bench','train', 'car', 'bicycle', 'truck', 'baseball glove', 'tennis racket', 'boat'] #You can give list of classes to filter by name, Be happy you don't have to put class number. ['train','person' ]
 
@@ -73,7 +71,7 @@ if (MODE == 'video'):
     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     nframes = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    
+
     # Initialzing object for writing video output
     output = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'DIVX'), fps , (w,h))
 else:   # realtime
@@ -123,7 +121,7 @@ with torch.no_grad():
       
       if ret:
         count +=1
-        img = letterbox(frame, imgsz, stride=stride)[0]
+        img = adjust_image_to_desired_shape(frame, imgsz, stride=stride)[0]
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
         img = np.ascontiguousarray(img)
         img = torch.from_numpy(img).to(device)
