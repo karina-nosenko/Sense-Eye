9bfaf69771be2c1b81c55d197b49701d6042eab4
karina-nosenko <karina4nosenko@gmail.com>
2023-02-23 21:56:40 +0200

    refactor

--- ImageProcessingUnit/objects_detection.py
+++ ImageProcessingUnit/objects_detection.py
@@ -6,11 +6,30 @@ from yolov7.utils.general import non_max_suppression, scale_coords, check_img_si
 from yolov7.utils.torch_utils import time_synchronized, select_device
 from yolov7.models.experimental import attempt_load
 
-from configs import options, MODE
+from configs import options
 from image_functions import adjust_image_to_desired_shape
 
 
 def initialize_player_detection_model():
+    """
+    Initializes the player detection model with the provided weights and configuration options.
+
+    Args:
+        None
+
+    Returns:
+        weights (str): The path of the weight file used for the model.
+        img_size (int): The image size used for the model.
+        device (torch.device): The device on which the model is running.
+        use_half_precision (bool): A boolean value indicating whether half precision is used for the model.
+        model (torch.nn.Module): The player detection model.
+        stride (int): The stride value used for the model.
+        names (list): The list of class names used for the model.
+        classes (list): The list of class indexes corresponding to the class names.
+
+    Raises:
+        None
+    """
     weights, img_size = options['weights'], options['img-size']
     device = select_device(options['device'])
     use_half_precision = device.type != 'cpu'
@@ -32,21 +51,83 @@ def initialize_player_detection_model():
     return weights, img_size, device, use_half_precision, model, stride, names, classes
 
 
-def find_nearest_player_to_the_ball(ball_center_point, all_detections, names):
+def detect_objects(frame, player_with_the_ball_center_point, img_size, device, use_half_precision, model, stride, names, classes):
     """
-    Find the nearest detected person to the ball center point.
+    Detects objects in a frame using a PyTorch model.
 
     Args:
-        ball_center_point (tuple): The center point of the ball in (x,y) format.
-        all_detections (list): A list of detected objects, where each object is represented by a tuple of coordinates, confidence score, and class id.
-        names (list): A list of class names, where the class id at each index corresponds to the class name at that index.
+        frame (numpy.ndarray): The frame to detect objects in.
+        player_with_the_ball_center_point (tuple of int): The center point of the player with the ball.
+        img_size (int): The size of the input image for the model.
+        device (str): The device to use for inference.
+        use_half_precision (bool): Whether to use half-precision or not.
+        model (torch.nn.Module): The PyTorch model to use for object detection.
+        stride (int): The stride used for adjusting the image shape.
+        names (list of str): The class names of the objects to detect.
+        classes (list of int): The class IDs of the objects to detect.
 
     Returns:
-        tuple: The coordinates of the bounding box (in x1, y1, x2, y2 format) of the nearest detected person to the ball center point, or None if no person is detected.
+        None
 
     Raises:
         None
     """
+    # Convert image to desired shape and format
+    img = adjust_image_to_desired_shape(frame, img_size, stride=stride)[0]
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
+    # Convert image to PyTorch tensor
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if use_half_precision else img.float()
+
+    # Normalize image pixel values to 0-1 range
+    img /= 255.0
+
+    # Add a forth dimention to the image object (batch_size=1)
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Inference
+    t1 = time_synchronized()
+    detection_results = model(img, augment= False)[0]        
+    detection_results = non_max_suppression(detection_results, options['conf-thres'], options['iou-thres'], classes=classes, agnostic= False)
+    t2 = time_synchronized()
+
+    # Plotting the detections
+    for detection in detection_results:
+        if not len(detection):
+            continue
+
+        # Scale box coordinates to the size of current frame
+        detection[:, :4] = scale_coords(img.shape[2:], detection[:, :4], frame.shape).round()
+   
+        player_with_the_ball_center_point = _detect_player_with_the_ball(detection, names)
+
+        # Draw the class name label and center point for each object
+        for i, (x1, y1, x2, y2, confidence_score, class_id) in enumerate(detection):
+            center_x = int((x1 + x2) // 2)
+            center_y = int((y1 + y2) // 2)  
+
+            # Assign color based on object class
+            class_name = names[int(class_id)]
+            if center_x == player_with_the_ball_center_point[0] and center_y == player_with_the_ball_center_point[1]:
+                text_color = (255, 255, 0)  # blue for player with the ball
+            elif class_name == 'sports ball':
+                text_color = (0, 255, 0)  # green for ball
+                class_name = 'ball'
+            else:
+                text_color = (0, 0, 255)  # red for rest of the players
+
+            # Create text label and display it on the frame
+            text_label = f'{class_name} ({center_x}, {center_y}) {confidence_score}'
+            cv2.putText(frame, text_label, (center_x, center_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 2)
+
+            # Draw center point circle on the frame
+            cv2.circle(frame, (center_x, center_y), 3, (0, 0, 255), -1)
+
+
+def _find_nearest_player_to_the_ball(ball_center_point, all_detections, names):
     if not ball_center_point:
         return None
 
@@ -70,69 +151,26 @@ def find_nearest_player_to_the_ball(ball_center_point, all_detections, names):
 
     return nearest_player_center_point if min_distance_to_ball != float('inf') else None
 
-def detect_objects(frame, player_with_the_ball_center_point, img_size, device, use_half_precision, model, stride, names, classes):
-    img = adjust_image_to_desired_shape(frame, img_size, stride=stride)[0]
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if use_half_precision else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
-    # Add a forth dimention to the image object - batch_size=1
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
-
-    # Inference
-    t1 = time_synchronized()
-    detection_results = model(img, augment= False)[0]        
-    detection_results = non_max_suppression(detection_results, options['conf-thres'], options['iou-thres'], classes=classes, agnostic= False)
-    t2 = time_synchronized()
-
-    # Plotting the detections
-    for i, det in enumerate(detection_results):
-        if len(det):
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], frame.shape).round()
-
-            # Detect the player with the ball - the player whose coordinates are
-            # the nearest to the found ball.
-            ball_found_in_current_frame = False
-            for *xyxy, confidence_score, class_id in reversed(det):
-                if (names[int(class_id)] == 'sports ball'):
-                    ball_found_in_current_frame = True
-                    center_point = ((xyxy[0] + xyxy[2]) // 2, (xyxy[1] + xyxy[3]) // 2)
-                    nearest_player_to_the_ball = find_nearest_player_to_the_ball(center_point, det, names)
-                    if nearest_player_to_the_ball:
-                        player_with_the_ball_center_point = ((nearest_player_to_the_ball[0] + nearest_player_to_the_ball[2]) // 2, (nearest_player_to_the_ball[1] + nearest_player_to_the_ball[3]) // 2)
-            
-            # If the ball was not found in the current frame - the 'player with the ball'
-            # will be the player whose coordinates are the nearest to the 'player with the ball'
-            # in the previous frame.
-            if not ball_found_in_current_frame:
-                for *xyxy, confidence_score, class_id in reversed(det):
-                    nearest_player_to_the_ball = find_nearest_player_to_the_ball(player_with_the_ball_center_point, det, names)
-                    if nearest_player_to_the_ball:
-                        player_with_the_ball_center_point = ((nearest_player_to_the_ball[0] + nearest_player_to_the_ball[2]) // 2, (nearest_player_to_the_ball[1] + nearest_player_to_the_ball[3]) // 2)
-
-            # Draw the class name label and center point for each object
-            for *xyxy, confidence_score, class_id in reversed(det):
-                center_point = ((xyxy[0] + xyxy[2]) // 2, (xyxy[1] + xyxy[3]) // 2)
-
-                # Class name and center point coordinates label
-                center_point_coordinates_label = f"({center_point[0]}, {center_point[1]})"
-                class_name_label = names[int(class_id)]  
-
-                if center_point == player_with_the_ball_center_point:
-                    text_color = (255, 255, 0) # blue for player with the ball
-                elif class_name_label == 'sports ball':
-                    text_color = (0, 255, 0) # green for ball
-                    class_name_label = 'ball'
-                else:
-                    text_color = (0, 0, 255) # red for rest of the players
-
-                text_label = f'{class_name_label} {center_point_coordinates_label} {confidence_score}'
-                cv2.putText(frame, text_label, tuple(map(int, (center_point[0], center_point[1] - 10))), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 2)
-
-                # Center point circle
-                cv2.circle(frame, tuple(map(int, center_point)), 3, (0, 0, 255), -1)
+def _detect_player_with_the_ball(detection, names):
+    # Detect the player with the ball - the player whose coordinates are
+    # the nearest to the found ball.
+    ball_found_in_current_frame = False
+    for *xyxy, confidence_score, class_id in reversed(detection):
+        if (names[int(class_id)] == 'sports ball'):
+            ball_found_in_current_frame = True
+            center_point = ((xyxy[0] + xyxy[2]) // 2, (xyxy[1] + xyxy[3]) // 2)
+            nearest_player_to_the_ball = _find_nearest_player_to_the_ball(center_point, detection, names)
+            if nearest_player_to_the_ball:
+                player_with_the_ball_center_point = ((nearest_player_to_the_ball[0] + nearest_player_to_the_ball[2]) // 2, (nearest_player_to_the_ball[1] + nearest_player_to_the_ball[3]) // 2)
+    
+    # If the ball was not found in the current frame - the 'player with the ball'
+    # will be the player whose coordinates are the nearest to the 'player with the ball'
+    # in the previous frame.
+    if not ball_found_in_current_frame:
+        for *xyxy, confidence_score, class_id in reversed(detection):
+            nearest_player_to_the_ball = _find_nearest_player_to_the_ball(player_with_the_ball_center_point, detection, names)
+            if nearest_player_to_the_ball:
+                player_with_the_ball_center_point = ((nearest_player_to_the_ball[0] + nearest_player_to_the_ball[2]) // 2, (nearest_player_to_the_ball[1] + nearest_player_to_the_ball[3]) // 2)
 
 
