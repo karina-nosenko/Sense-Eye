8f88aa8341dc548cf05a3d8482f321128ebd3159
YosefAranbaev <aranbaev00@gmail.com>
2023-03-04 20:51:07 +0200

    fixing sightDirection bug

--- ImageProcessingUnit/objects_detection.py
+++ ImageProcessingUnit/objects_detection.py
@@ -164,6 +164,7 @@ def detect_objects(frame, prev_person_center_points, player_with_the_ball_center
                     "holdsBall": False,
                     "x": center_x,
                     "y": center_y
+                    "sightDirection": float(angles[i])
                 }
                 players_list_indexes_direction_playerWithTheBasll.append(
                     player)
