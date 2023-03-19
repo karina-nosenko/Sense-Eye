2150b0a0fd47fa936b42e68a56f8b0deee65d56e
karina-nosenko <karina4nosenko@gmail.com>
2023-03-19 15:43:02 +0200

    fix yaml

--- run_config_file.py
+++ run_config_file.py
@@ -15,17 +15,20 @@ with open('configuration.yaml', 'r', encoding='utf-8') as f:
 # Run the Node.js server
 # subprocess.run(["npm", "run", "dev"], shell=True)
 # print([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],config['node']['working_directory'])
+os.system("sudo fuser -k 5000/tcp")
+os.system("sudo fuser -k 8080/tcp")
 
 process1 = subprocess.Popen([config['python']['sudo_command'], config['python']['command'], config['python']['file_name']])
 process2 = subprocess.Popen([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],cwd=config['node']['working_directory'])
-# process3 = subprocess.Popen([config['video']['sudo_command'], config['video']['command'], config['video']['file_name']])
+process3 = subprocess.Popen([config['video']['sudo_command'], config['video']['command'], config['video']['file_name']],cwd=config['video']['working_directory'])
+# process3 = subprocess.Popen(['sudo','python3','main.py'],cwd='ImageProcessingUnit/')
 
-process1.communicate()
-process2.communicate()
-# process3.communicate()
+# process1.communicate()
+# process2.communicate()
+# # process3.communicate()
 
-process1.terminate()
-process2.terminate()
+# process1.terminate()
+# process2.terminate()
 # process3.terminate()
 
 # pid1 = process1.pid
