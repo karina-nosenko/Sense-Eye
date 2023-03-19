4f1751a65a350cf34e2d6a03d4200d34f4bbe247
karina-nosenko <karina4nosenko@gmail.com>
2023-03-19 15:24:55 +0200

    update yaml

--- run_config_file.py
+++ run_config_file.py
@@ -2,6 +2,7 @@ import yaml
 import subprocess
 import subprocess
 import os
+import signal
 # Read the configuration file
 with open('configuration.yaml', 'r', encoding='utf-8') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
@@ -13,6 +14,24 @@ with open('configuration.yaml', 'r', encoding='utf-8') as f:
 
 # Run the Node.js server
 # subprocess.run(["npm", "run", "dev"], shell=True)
-print([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],config['node']['working_directory'])
-subprocess.Popen([config['python']['command'], config['python']['file_name']],shell = True)
-subprocess.Popen([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],cwd=config['node']['working_directory'],shell=True)
+# print([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],config['node']['working_directory'])
+
+process1 = subprocess.Popen([config['python']['sudo_command'], config['python']['command'], config['python']['file_name']])
+process2 = subprocess.Popen([config['node']['command'], config['node']['argument_1'], config['node']['argument_2']],cwd=config['node']['working_directory'])
+# process3 = subprocess.Popen([config['video']['sudo_command'], config['video']['command'], config['video']['file_name']])
+
+process1.communicate()
+process2.communicate()
+# process3.communicate()
+
+process1.terminate()
+process2.terminate()
+# process3.terminate()
+
+# pid1 = process1.pid
+# pid2 = process2.pid
+# pid3 = process3.pid
+
+# os.kill(pid1, signal.SIGTERM)
+# os.kill(pid2, signal.SIGTERM)
+# os.kill(pid3, signal.SIGTERM)
