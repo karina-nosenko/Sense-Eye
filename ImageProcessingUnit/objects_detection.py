878c517281b94a734717647e31ea0da298a3d086
root <root@MSI>
2023-03-25 13:30:19 +0300

    matching colors to the top coordinate of the person instead of the center

--- ImageProcessingUnit/objects_detection.py
+++ ImageProcessingUnit/objects_detection.py
@@ -194,7 +194,7 @@ def detect_objects(frame, prev_person_center_points, player_with_the_ball_center
                 player = {
                     "holdsBall": True,
                     "x": center_x,
-                    "y": center_y,
+                    "y": int(y1),
                     "sightDirection": float(angles[i])
                 }
                 players_list_indexes_direction_playerWithTheBasll.append(
@@ -204,7 +204,7 @@ def detect_objects(frame, prev_person_center_points, player_with_the_ball_center
                 player = {
                     "holdsBall": False,
                     "x": center_x,
-                    "y": center_y,
+                    "y": int(y1),
                     "sightDirection": float(angles[i])
                 }
                 players_list_indexes_direction_playerWithTheBasll.append(
