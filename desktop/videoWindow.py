2f25338fe4ddcfc11e087ef1071863ddaeebd652
karina-nosenko <karina4nosenko@gmail.com>
2023-04-30 20:33:06 +0300

    imports

--- desktop/videoWindow.py
+++ desktop/videoWindow.py
@@ -2,6 +2,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 import os
 
 
