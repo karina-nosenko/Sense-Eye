433005a4d4a0be49a9ea8cc93355551ed4a6972b
karina-nosenko <karina4nosenko@gmail.com>
2023-05-13 16:41:58 +0300

    integration finished

--- desktop/personalized_frames.py
+++ desktop/personalized_frames.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import defaultdict
+from PIL import Image
 
 defined_strings = {0: 'orange', 1: 'pink'}
 
@@ -29,27 +30,29 @@ def create_personalized_frames():
         # Open the JSON file and load its contents into a Python object
         with open(trace_file_path, 'r') as f:
             data = json.load(f)
-        df = pd.DataFrame.from_dict(data, orient='columns')
-        df = df.dropna(subset=['gameId'])
-
-        # Fiter the df to include only players
-        filtered_data = [obj for obj in data if obj.get('class') == 'person' and 'properties' in obj and all(key in obj['properties'] for key in ['holdsBall', 'x', 'y', 'sightDirection', 'center_y', 'id', 'team'])]
-        filtered_df = pd.DataFrame(filtered_data)
-
-        # Create a copy of the original DataFrame
-        players_df = df.copy()
-
-        if 'properties' in players_df and 'id' in players_df['properties']:
-            players_df['properties']['id'] = players_df['properties']['id'].astype(str).apply(lambda x: defined_strings.get(int(x), x))
 
         # Create the frames with insights
-        create_ball_holders_percentages(game_path, df)
-        create_correlations(game_path, filtered_df)
+        create_ball_holders_percentages(data).savefig(game_path + '/ball_holders.png')
+        create_correlations(data).savefig(game_path + '/correlations.png')
+        create_ball_movement_pattern(data).savefig(game_path + '/ball_movement.png')
+        create_player_movement_pattern(game_path, data).savefig(game_path + '/players_movement.png')
+        create_players_heatmap(game_path, data).savefig(game_path + '/players_heatmap.png')   #todo: flip vertically
+        create_ball_heatmap(game_path, data).savefig(game_path + '/ball_heatmap.png')   #todo: flip vertically
 
-def create_ball_holders_percentages(game_path, df):
+def create_ball_holders_percentages(data):
     default_color = 'gray'  # Default color for IDs not found in defined_strings
     next_numeric_id = 2  # Start assigning numeric IDs from 3 onwards
 
+    # Load content into dataframe
+    df = pd.DataFrame.from_dict(data, orient='columns')
+    df = df.dropna(subset=['gameId'])
+
+    # Create a copy of the original DataFrame
+    players_df = df.copy()
+
+    if 'properties' in players_df and 'id' in players_df['properties']:
+        players_df['properties']['id'] = players_df['properties']['id'].astype(str).apply(lambda x: defined_strings.get(int(x), x))
+    
     # Step 1: Group objects by gameId
     game_groups = defaultdict(list)
     for _, row in df.iterrows():
@@ -113,9 +116,15 @@ def create_ball_holders_percentages(game_path, df):
         # Adjust the layout to ensure the text fits within the figure
         plt.tight_layout()
 
-        plt.savefig(game_path + '/ball_holders.png')
+        return plt
 
-def create_correlations(game_path, filtered_df):
+def create_correlations(data):
+    plt.figure()
+
+    # Fiter the df to include only players
+    filtered_data = [obj for obj in data if obj.get('class') == 'person' and 'properties' in obj and all(key in obj['properties'] for key in ['holdsBall', 'x', 'y', 'sightDirection', 'center_y', 'id', 'team'])]
+    filtered_df = pd.DataFrame(filtered_data)
+
     # Create a DataFrame from the filtered data
     df = pd.DataFrame(filtered_df)
 
@@ -138,4 +147,136 @@ def create_correlations(game_path, filtered_df):
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', mask=mask)
     plt.title('Correlation Heatmap')
 
-    plt.savefig(game_path + '/correlations.png')
+    return plt
+
+def create_ball_movement_pattern(data):
+    plt.figure()
+
+    # Extract the x-y coordinates for each ball location
+    coords = []
+    for d in data:
+        if d["class"] == "ball":
+            coords.append([d["properties"]["x"], d["properties"]["y"]])
+
+    # Create a heatmap using Matplotlib
+    if len(coords) > 0:
+        coords = np.array(coords)
+        plt.hist2d(coords[:, 0], coords[:, 1], bins=30, cmap=plt.cm.jet)
+
+    plt.colorbar()
+    plt.title("Ball Location Heatmap")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+    return plt
+
+def create_player_movement_pattern(game_path, data):
+    plt.figure()
+
+    # Load the background image
+    img = Image.open(game_path + '/first_frame.jpg')
+
+    # Create a figure and axis objects with the same size as the background image
+    fig, ax = plt.subplots(figsize=(img.width/100, img.height/100))
+
+    # Plot the background image
+    ax.imshow(img)
+
+    # Set the x and y limits of the axis to match the dimensions of the image
+    ax.set_xlim([0, img.width])
+    ax.set_ylim([img.height, 0])
+
+    # Group the data by gameId
+    groups = {}
+    for d in data:
+        gameId = d["gameId"]
+        if gameId not in groups:
+            groups[gameId] = []
+        if d["class"] == "person":
+            groups[gameId].append([d["properties"]["x"], d["properties"]["y"]])
+
+    # Create a heatmap for each group
+    for gameId, coords in groups.items():
+        if len(coords) > 0:
+            coords = np.array(coords)
+            # Set the extent of the heatmap to match the image dimensions
+            im = ax.hist2d(coords[:, 0], coords[:, 1], bins=30, cmap=plt.cm.jet)[3]
+        plt.colorbar(im, ax=ax)
+        ax.set_title(f"Players Location Heatmap")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+
+        return plt
+    
+def create_players_heatmap(game_path, data):
+    plt.figure()
+
+    # Load the background image
+    img = Image.open(game_path + '/first_frame.jpg')
+
+    # Create a figure and axis objects with a larger size
+    fig, ax = plt.subplots(figsize=(16, 8))
+    # Set the aspect ratio of the image to match the dimensions of the plot
+    aspect = img.width / float(img.height)
+    ax.set_aspect(aspect)
+
+    # Plot the background image
+    ax.imshow(img)
+
+    # Group the data by gameId
+    groups = {}
+    for d in data:
+        gameId = d["gameId"]
+        if gameId not in groups:
+            groups[gameId] = []
+        if d["class"] == "person":
+            groups[gameId].append([d["properties"]["x"], d["properties"]["y"]])
+
+    # Create a heatmap for each group
+    for gameId, coords in groups.items():
+        if len(coords) > 0:
+            coords = np.array(coords)
+            # Set the range of the heatmap to match the image dimensions
+            im, _, _, _ = ax.hist2d(coords[:, 0], coords[:, 1], bins=30, cmap=plt.cm.Reds, alpha=0.4, range=[[0, img.width], [0, img.height]])
+        ax.set_title(f"Players Location Heatmap")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+
+        return plt
+    
+def create_ball_heatmap(game_path, data):
+    plt.figure()
+
+    # Load the background image
+    img = Image.open(game_path + '/first_frame.jpg')
+
+    # Create a figure and axis objects with a larger size
+    fig, ax = plt.subplots(figsize=(16, 8))
+
+    # Set the aspect ratio of the image to match the dimensions of the plot
+    aspect = img.width / float(img.height)
+    ax.set_aspect(aspect)
+
+    # Plot the background image
+    ax.imshow(img)
+
+    # Group the data by gameId
+    groups = {}
+    for d in data:
+        gameId = d["gameId"]
+        if gameId not in groups:
+            groups[gameId] = []
+        if d["class"] == "ball":
+            groups[gameId].append([d["properties"]["x"], d["properties"]["y"]])
+
+    # Create a heatmap for each group
+    for gameId, coords in groups.items():
+        if len(coords) > 0:
+            coords = np.array(coords)
+            # Set the range of the heatmap to match the image dimensions
+            im, _, _, _ = ax.hist2d(coords[:, 0], coords[:, 1], bins=30, cmap=plt.cm.Reds, alpha=0.4, range=[[0, img.width], [0, img.height]])
+        ax.set_title(f"Ball's Location Heatmap")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+
+        return plt
