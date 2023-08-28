88099a5c1fefcc444e996e5b2c4cd2124314b5fd
Karina Nosenko <77194094+karina-nosenko@users.noreply.github.com>
2023-08-28 14:56:10 +0300

    Update mainWindow.py

--- desktop/mainWindow.py
+++ desktop/mainWindow.py
@@ -26,12 +26,6 @@ import os
 # Load the .env file
 load_dotenv()
 
-# def rescale_frame(frame, scale):
-#     width = int(frame.shape[1] * scale)
-#     height = int(frame.shape[0] * scale)
-#     dimensions = (width, height)
-#     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
-
 # == Login Page ==#
 class LoginPage(QMainWindow):
     def __init__(self):
